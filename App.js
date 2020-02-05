256d7a998b519ecfc6ed30f57541787a9090733b
stealthanthrax <sansyrox@gmail.com>
2020-02-06 03:22:04 +0530

    FInal done

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'; // hooks
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+import {uuid} from 'uuidv4';
+
+const App = () => {
+  const [items, setItems] = useState([  // items is the state store and setItems is the function which uses it
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id)
+    });
+  }
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Pleasse enter the Item', {text: 'Ok'});
+    }
+    else {
+      setItems(prevItems => {
+        return [{id: uuid(), text:text}, ...prevItems]
+      });
+    }
+    
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header  title="Shopping List"/>
+      <AddItem addItem={addItem}/>
+      {/* <Image source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'} style={styles.img} /> */}
+      <FlatList data={items} renderItem={({item})=> <ListItem item={item} deleteItem={deleteItem}/>
+      }/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    paddingTop: 60
+  }
+});
+
+export default App;
